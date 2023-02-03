8b994ce40186d189685fbbb3bb9b417f560d7482
Sarah <sarah@cleancodecompany.com>
2023-02-03 21:28:13 +0000

    Lab 2, Task 1, LINQ qureies complete

--- Fleet/Fleet/Program.cs
+++ Fleet/Fleet/Program.cs
@@ -5,50 +5,80 @@ using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 Car Vech1 = new Car("Nissan", "Leaf", "231-D-007", 1200);
 Car Vech2 = new Car("Mazda", "Red", "211-C-4044", 1600);
-Car Vech3 = new Car("Nissan", "Leaf", "231-D-007", 1600);
+Car Vech3 = new Car("Nissan", "Leaf", "231-D-8707", 1600);
 Car Vech4 = new Car("Tesla", "S-Series", "231-D-902", 2200);
 Car Vech5 = new Car("Mazda", "Red", "221-K-9044", 1600);
 Car Vech6 = new Car("Tesla", "S-Series", "212-D-2656", 1200);
 Car Vech7 = new Car("Tesla", "S-Series", "222-D-650", 1200);
-Car Vech8 = new Car("Nissan", "Leaf", "231-D-007", 1200);
+Car Vech8 = new Car("Nissan", "Leaf", "231-D-777", 1200);
 Car Vech9 = new Car("Nissan", "Leaf", "231-D-7090", 1200);
 Car Vech10 = new Car("Nissan", "Leaf", "231-D-8722", 1800);
 
-Console.WriteLine(Vech1);
-
-CarContext ctx = new CarContext();
-
-var x = ctx.Database.EnsureCreated();
-ctx.Cars.Add(Vech1);
-ctx.Cars.Add(Vech2);
-ctx.Cars.Add(Vech3);
-ctx.Cars.Add(Vech4);
-ctx.Cars.Add(Vech5);
-ctx.Cars.Add(Vech6);
-ctx.Cars.Add(Vech7);  
-ctx.Cars.Add(Vech8);
-ctx.Cars.Add(Vech9);
-ctx.Cars.Add(Vech10); 
-    
-ctx.SaveChanges();
 
 
-// Student collection
-IList<Car> FleetList = new List<Car>() 
-{ 
-        new Car("Nissan", "Leaf", "231-D-007", 1200),
-        new Car("Mazda", "Red", "211-C-4044", 1600),
-        new Car("Nissan", "Leaf", "231-D-007", 1600),
-        new Car("Tesla", "S-Series", "231-D-902", 2200),
-        new Car("Mazda", "Red", "221-K-9044", 1600),
-        new Car("Tesla", "S-Series", "212-D-2656", 1200),
-        new Car("Tesla", "S-Series", "222-D-650", 1200),
-        new Car("Nissan", "Leaf", "231-D-007", 1200),
-        new Car("Mazda", "Blue", "231-D-7090", 1200),
-        new Car("Mazda", "Silver", "231-D-8722", 1800)
+
+// connect to local database
+var context = new CarContext();
+context.Database.EnsureCreated();
+
+
+// function to add a Car to the loacl database if it is not already there
+var addCar = (Car c) => {
+      var result = context.Cars.Find(c.Registration);
+    if (result == null) {
+        context.Cars.Add(c);
+    }
+    else
+    {
+        Console.WriteLine(c.Registration + " That Registration Already Exists... Skipping...");
+    }
 };
 
-// LINQ Query  to find out Mazda's with EngineSize
-var Mazdas = from c in FleetList
-                      where c.EngineSize < 1600 && c.Model = "Mazda"
-                      select c;
+addCar(Vech1);
+addCar(Vech2);
+addCar(Vech3);
+addCar(Vech4);
+addCar(Vech5);
+addCar(Vech6);
+addCar(Vech7);
+addCar(Vech8);
+addCar(Vech9);
+addCar(Vech10);
+
+context.SaveChanges();
+
+// List all cars in ascending registration order
+var AscOrder = from c in context.Cars
+               orderby c.Registration
+               select c;
+Console.WriteLine(AscOrder);
+
+
+// List the models for all Mazda cars in the fleet
+var Mazdas = from c in context.Cars
+    where c.Model == "Mazda"
+    select c;
+
+
+// List all cars in descending engine size order
+var DescOrder = from c in context.Cars
+                orderby c.EngineSize descending
+                select c;
+
+
+// List just the make and model for cars whose engine size is 1600 cc
+var MakenModel = from c in context.Cars
+                 where c.EngineSize == 1600
+                 select c.Make + "//"+ c.Model;
+
+
+// Count the number of cars whose engine size is 1600 cc or less
+var CountingSmallCars = (from c in context.Cars
+                        where c.EngineSize <= 1600
+ 
+                         
+                         select c).Count();
+
+
+Console.WriteLine("\n\tTask 1 Complete");
+Console.ReadLine();
