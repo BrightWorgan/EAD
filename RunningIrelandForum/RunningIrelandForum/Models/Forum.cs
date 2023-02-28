f03384872586f581c96b986632f307562287d382
Sarah <sarah@cleancodecompany.com>
2023-02-28 22:11:49 +0000

    Start of Exercise 1 / discussion forum excersise

---
+++ RunningIrelandForum/RunningIrelandForum/Models/Forum.cs
@@ -0,0 +1,12 @@
+﻿namespace RunningIrelandForum.Models
+{
+    public static class Forum
+    {
+        public static List<UserPost> ForumHome = new List<UserPost>();
+
+        public static void init()
+        {
+            UserPost up1 = new() { Subject = "Blessington Greenway", Message = "New Bridge is intalled and is a wonderful edition", Timestamp = new DateTime(1979, 07, 28, 22, 35, 5,) };
+        }
+    }
+}
---
+++ RunningIrelandForum/RunningIrelandForum/Models/UserPost.cs
@@ -0,0 +1,19 @@
+﻿namespace RunningIrelandForum.Models
+{
+    public class UserPost
+    {
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public UserPost(int id, string Subject, string Message, DateTime Timestamp)
+        {
+            this.Subject = Subject;
+            this.Message = Message;
+            this.Timestamp = Timestamp;
+        }
+
+
+
+    }
+}
