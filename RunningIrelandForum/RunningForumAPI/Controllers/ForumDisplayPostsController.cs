760d66d1c328f29e3904965ba13b725df3b5b640
Sarah <sarah@cleancodecompany.com>
2023-03-06 17:56:34 +0000

    Lab complete

--- RunningIrelandForum/RunningForumAPI/Controllers/ForumDisplayPostsController.cs
+++ RunningIrelandForum/RunningForumAPI/Controllers/ForumDisplayPostsController.cs
@@ -12,38 +12,23 @@ namespace RunningForumAPI.Controllers
         [HttpGet(Name = "ForumDisplayPostsController")]
         public List<UserPost> Get(int UserNum)
         {
-            //List<UserPost> clonedListUserPost = new List<UserPost>(Forum.ForumHome);
+            //
             List<UserPost> posts = new List<UserPost>();
             int list_count = Forum.ForumHome.Count;
-            if (list_count < UserNum)
+            if (list_count <= UserNum)
             {
                 return Forum.ForumHome;
             }
-            if (list_count > UserNum)
-            {
+
                 for (int i = 1; i < UserNum +1; i++)
                 {
                     posts.Add(Forum.ForumHome[list_count-i]);
 
                 }
                 return posts;
-            
-            }
-            return null;
-
-
-            //foreach (var UserPost in Forum.ForumHome)
-            //{
-            //    if (clonedListUserPost.length()
-            //    {
-            //        return UserPost;
-            //    }
-            //}
-            //return null;
 
         }
-        //List<string> source = new List<string>() { "A", "B", "C" };
-        //List<UserPost> clonedListUserPost = new List<UserPost>(source);
-        //Console.WriteLine(String.Join(",", clonedList));
+        
+
     }
 }
