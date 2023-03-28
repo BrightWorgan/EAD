205781aa72c77e5d1670ab98b8a425c9716682f4
Sarah <sarah@cleancodecompany.com>
2023-03-28 23:02:48 +0100

    EAD exercise 1 part 1 and two

---
+++ EADExercise1AzureCloudService/EADExercise1AzureCloudService/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using EADExercise1AzureCloudService.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Diagnostics;
+
+namespace EADExercise1AzureCloudService.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: HomeController/Create
+        public ActionResult CalculateServiceCost()
+        {
+            ViewBag.InstanceSize = new SelectList(Instance.InstanceTypes);
+            return View(new Instance() { NumOfInstances = 2 });
+        }
+
+      
+
+
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EADExercise1AzureCloudService/EADExercise1AzureCloudService/Models/Instance.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace EADExercise1AzureCloudService.Models
+{
+    public class Instance
+    {
+        const double VerySmallCost = 0.02;
+        const double SmallCost = 0.08;
+        const double MediumCost = 0.16;
+        const double LargeCost = 0.32;
+        const double VeryLargeCost = 0.64;
+        const double A6Cost = 0.90;
+        const double A7Cost = 1.80;
+
+
+        public static String[] InstanceTypes
+        {
+            get
+            {
+                return new String[] { "VerySmall", "Small", "Medium", "Large", "VeryLarge", "A6", "A7" };
+            }
+        }
+
+        public String InstanceType { get; set;}
+
+        [Required(ErrorMessage = "*Required Field!*")]
+        [DisplayName("Instance Size: ")]
+        public string InstanceSize { get; set; }
+
+        [Required(ErrorMessage = "* Required field! *")]
+        [DisplayName("Number of Instances:")]
+        [Range(2, Int32.MaxValue, ErrorMessage = "* ERROR!: Please Select at least two instances *")]
+
+        public int NumOfInstances { get; set; }
+
+
+        public double AnnualCost
+        {
+            get
+            {
+                double cost = 0;
+
+                double year = 365;
+                if (DateTime.IsLeapYear(DateTime.Now.Year))
+                {
+                    return year = 366;
+                }
+
+                if (InstanceType == "VerySmall")
+                {
+                    return VerySmallCost * 24 * year;
+                }
+                else if (InstanceType == "Small")
+                {
+                    return SmallCost * 24 * year;
+                }
+                else if (InstanceType == "Medium")
+                {
+                    return MediumCost * 24 * year;
+                }
+                else if (InstanceType == "Large")
+                {
+                    return LargeCost * 24 * year;
+                }
+                else if (InstanceType == "VeryLarge")
+                {
+                    return VeryLargeCost * 24 * year;
+                }
+                else if (InstanceType == "A6")
+                {
+                    return A6Cost * 24 * year;
+                }
+                else if (InstanceType == "A7")
+                {
+                    return A7Cost * 24 * year;
+                }
+
+                return cost;
+            }
+        }
+
+    }
+
+}    
+// hr cost x 24 x 365.25 = annual cost
